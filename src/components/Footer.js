5adef959c906788b364e7f79c2d8c49e29705ec4
latilanoh@gmail.com <latilanoh@gmail.com>
2018-08-16 10:59:00 -0500

    subiendo materialize con react

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "../index.css";
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer>
+                <div className="page-footer cyan darken-4 center">
+                    © Luly Ecommerce App 2018
+                </div>
+            </footer>
+        )
+    }
+};
+
+export default Footer;
